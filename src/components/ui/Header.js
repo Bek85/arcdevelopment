0c04e4492455d62991f6c5740aee79197bf11559
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-11-04 16:19:21 +0500

    Import company logo in svg format and place it in the header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -21,6 +22,10 @@ function ElevationScroll(props) {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: {
+    height: '7em',
   },
 }));
 
@@ -31,8 +36,8 @@ export default function Header(props) {
     <React.Fragment>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
-          <Toolbar>
-            <Typography variant='h3'>Arc Development</Typography>
+          <Toolbar disableGutters>
+            <img className={classes.logo} src={logo} alt='company logo' />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
