166658990f7336201e3d98d9b4c1bee08642fb27
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-11-04 19:33:34 +0500

    Integrate react-router service for app navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { theme } from './ui/Theme';
 import Header from './ui/Header';
@@ -6,7 +7,32 @@ import Header from './ui/Header';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route exact path='/services' component={() => <div>Services</div>} />
+          <Route
+            exact
+            path='customsoftware/'
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path='/mobileapps'
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path='/websites' component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path='/revolution'
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path='/about' component={() => <div>About</div>} />
+          <Route exact path='/contact' component={() => <div>Contact</div>} />
+          <Route exact path='/estimate' component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
       Other Components go here...
     </ThemeProvider>
   );
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -64,14 +65,41 @@ export default function Header(props) {
               className={classes.tabContainer}
               indicatorColor='primary'
             >
-              <Tab className={classes.tab} label='Home' />
-              <Tab className={classes.tab} label='Services' />
-              <Tab className={classes.tab} label='Revolution' />
-              <Tab className={classes.tab} label='About Us' />
-              <Tab className={classes.tab} label='Contact Us' />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='contact'
+                label='Contact Us'
+              />
             </Tabs>
             <Button
               className={classes.btn}
+              component={Link}
+              to='estimate'
               variant='contained'
               color='secondary'
             >
