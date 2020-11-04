2227ca8d831f6f23bae34913aa3b6df7c72e176f
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-11-04 06:12:54 +0500

    Import ThemeProvider component from mui and create custom mui theme to override defaults

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from './ui/Theme';
 import Header from './ui/Header';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
-      {[...new Array(50)]
-        .map(
-          () =>
-            `Sadipscing sadipscing vero justo dolor ipsum est stet clita ea voluptua, voluptua et dolor amet justo consetetur dolor diam sit. Amet sed sit sed accusam sed, amet takimata amet tempor accusam, dolore amet est ea erat, vero ipsum erat stet sed rebum et accusam dolore sed. Takimata labore tempor rebum.`
-        )
-        .join('\n')}
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const arcBlue = '#0b72b9';
+const arcOrange = '#ffba60';
+
+export const theme = createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+});
